9f7fb115be64bb08af6a6ebb45da0ad6c86d909b
Robin Vobruba <hoijui.quaero@gmail.com>
2025-05-27 21:31:34 +0200

    Cleanup & improve main Error

--- src/error.rs
+++ src/error.rs
@@ -18,29 +18,31 @@ pub enum Error {
     #[error("Input is not equivalent to the (re-)formatted version of its self: {0}")]
     Check(String),
 
-    #[error("We do not support more then one base IRI defined per file")]
+    #[error(
+        "We do not support more then one base IRI defined per file.
+Consider refactoring the input first."
+    )]
     MultipleBases,
 
     #[error(transparent)]
     TurtleSyntaxError(#[from] oxttl::TurtleSyntaxError),
 
-    /// Represents all cases of `std::io::Error`.
     #[error(transparent)]
     Format(#[from] std::fmt::Error),
 
-    #[error("The target to format {0} does not seem to exist")]
+    #[error("The target file to format does not seem to exist: '{0}'")]
     TargetFileDoesNotExist(PathBuf),
 
-    #[error("Error while reading {0}")]
+    #[error("Error while reading file: '{0}'")]
     FailedToReadTargetFile(PathBuf),
 
     #[error("Failed to parse input as turtle: {0}")]
     ParseError(#[from] parser::Error),
 
-    #[error("Error while writing {0}")]
+    #[error("Error while writing to file: '{0}'")]
     FailedToWriteFormattedFile(#[source] std::io::Error, PathBuf),
 
-    #[error("Failed to list files in input directory {0}: {1:?}")]
+    #[error("Failed to list files in input directory '{0}': {1:?}")]
     FailedToListFilesInInputDir(#[source] std::io::Error, PathBuf, FilesListErrorType),
 
     #[error("Failed to create Turtle file tree structure: {0}")]
