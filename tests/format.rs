3e7aded4150d75675277ae22fa38f69a356c7bcb
Robin Vobruba <hoijui.quaero@gmail.com>
2025-06-16 17:09:22 +0200

    Re-enable unit-tests

--- tests/format.rs
+++ tests/format.rs
@@ -1,77 +1,68 @@
-// // SPDX-FileCopyrightText: 2023 Helsing GmbH
-// //
-// // SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2023 Helsing GmbH
+//
+// SPDX-License-Identifier: Apache-2.0
 
-// #[cfg(test)]
-// use pretty_assertions::assert_eq;
-// use turtlefmt::{format_turtle, options::FormatOptions};
+use std::rc::Rc;
 
-// type BoxResult<T> = Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
+#[cfg(test)]
+use pretty_assertions::assert_eq;
+use prttl::{error::Error, formatter::format, options::FormatOptions, parser};
 
-// fn fmt_opts_inverted() -> FormatOptions {
-//     FormatOptions {
-//         indentation: 2,
-//         sort_terms: true,
-//         new_lines_for_easy_diff: true,
-//         single_object_on_new_line: true,
-//         cleanup_dividing_empty_lines: true,
-//         force: true,
-//         prtyr_sorting: false,
-//     }
-// }
+fn fmt_opts_strict(single_object_on_new_line: bool) -> FormatOptions {
+    FormatOptions {
+        indentation: "  ".to_string(),
+        single_leafed_new_lines: single_object_on_new_line,
+        force: false,
+        prtr_sorting: true,
+        check: false,
+        sparql_syntax: false,
+        max_nesting: true,
+        canonicalize: true,
+        warn_unsupported_numbers: true,
+    }
+}
 
-// fn fmt_diff_human_mix_opt() -> FormatOptions {
-//     FormatOptions {
-//         indentation: 2,
-//         sort_terms: true,
-//         new_lines_for_easy_diff: true,
-//         single_object_on_new_line: false,
-//         cleanup_dividing_empty_lines: true,
-//         force: true,
-//         prtyr_sorting: true,
-//     }
-// }
+fn format_turtle(original: &str, options: FormatOptions) -> Result<String, Error> {
+    let options = Rc::new(options);
+    let input = parser::parse(original.as_bytes(), &options)?;
+    format(&input, options)
+}
 
-// #[test]
-// fn test_format() {
-//     let input = include_str!("from.simple.ttl");
-//     let expected = include_str!("to.simple.ttl");
-//     assert_eq!(
-//         format_turtle(input, &FormatOptions::default()).unwrap(),
-//         expected
-//     );
-// }
+#[test]
+fn test_format() -> Result<(), Error> {
+    let input = include_str!("data/input/pretty_printing/simple.ttl");
+    let expected = include_str!("data/output/pretty_printing/simple.ttl");
+    assert_eq!(format_turtle(input, FormatOptions::default())?, expected);
+    Ok(())
+}
 
-// #[test]
-// fn test_stable() {
-//     let file = include_str!("to.simple.ttl");
-//     assert_eq!(
-//         format_turtle(file, &FormatOptions::default()).unwrap(),
-//         file
-//     );
-// }
+#[test]
+fn test_stable() -> Result<(), Error> {
+    let file = include_str!("data/output/pretty_printing/simple.ttl");
+    assert_eq!(format_turtle(file, FormatOptions::default())?, file);
+    Ok(())
+}
 
-// #[test]
-// fn test_format_default_inverted() {
-//     let input = include_str!("from.simple.ttl");
-//     let expected = include_str!("to.simple.default_inverted.ttl");
-//     let format_options = fmt_opts_inverted();
-//     assert_eq!(format_turtle(input, &format_options).unwrap(), expected);
-// }
+#[test]
+fn test_format_strict() -> Result<(), Error> {
+    let input = include_str!("data/input/pretty_printing/simple.ttl");
+    let expected = include_str!("data/output/pretty_printing/simple.strict.ttl");
+    assert_eq!(format_turtle(input, fmt_opts_strict(true))?, expected);
+    Ok(())
+}
 
-// #[test]
-// fn test_stable_default_inverted() {
-//     let file = include_str!("to.simple.default_inverted.ttl");
-//     let format_options = fmt_opts_inverted();
-//     assert_eq!(format_turtle(file, &format_options).unwrap(), file);
-// }
+#[test]
+fn test_stable_default_inverted() -> Result<(), Error> {
+    let file = include_str!("data/output/pretty_printing/simple.strict.ttl");
+    assert_eq!(format_turtle(file, fmt_opts_strict(true))?, file);
+    Ok(())
+}
 
-// #[test]
-// fn test_blank_nodes_prtyr() -> BoxResult<()> {
-//     let input = include_str!("from.blank_nodes_prtyr.ttl");
-//     // let input = include_str!("from.simple.ttl");
-//     let expected = include_str!("to.blank_nodes_prtyr.ttl");
-//     let format_options = fmt_diff_human_mix_opt();
-//     assert_eq!(format_turtle(input, &format_options)?, expected);
-//     Ok(())
-// }
+#[test]
+fn test_blank_nodes_prtr() -> Result<(), Error> {
+    let input = include_str!("data/input/pretty_printing/blank_nodes_prtr.ttl");
+    let expected = include_str!("data/output/pretty_printing/blank_nodes_prtr.ttl");
+    // assert_eq!(format_turtle(input, FormatOptions::default())?, expected);
+    assert_eq!(format_turtle(input, fmt_opts_strict(false))?, expected);
+    Ok(())
+}
--- tests/w3c_testsuite.rs
+++ tests/w3c_testsuite.rs
@@ -1,21 +1,77 @@
-// // SPDX-FileCopyrightText: 2023 Helsing GmbH
-// //
-// // SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2023 Helsing GmbH
+//
+// SPDX-License-Identifier: Apache-2.0
 
-// use anyhow::{bail, Context, Result};
-// use oxrdf::graph::CanonicalizationAlgorithm;
-// use oxrdf::vocab::rdf;
-// use oxrdf::{Graph, NamedNodeRef, SubjectRef, TermRef};
-// use oxttl::TurtleParser;
-// use std::collections::hash_map::DefaultHasher;
-// use std::hash::Hasher;
-// use std::path::Path;
-// use std::{fs, str};
-// use turtlefmt::{format_turtle, options::FormatOptions};
+use oxrdf::graph::CanonicalizationAlgorithm;
+use oxrdf::vocab::rdf;
+use oxrdf::{Graph, NamedNodeRef, NamedOrBlankNodeRef, TermRef};
+use oxttl::TurtleParser;
+use prttl::{
+    error::Error as FmtError, formatter::format, options::FormatOptions, parser,
+    parser::Error as ParsingError,
+};
+use std::rc::Rc;
+use std::sync::LazyLock;
+use std::{fs, str};
+use thiserror::Error;
 
-// const CACHE: &str = "tests/cache";
+static NN_W3C_TESTS_ACTION: LazyLock<NamedNodeRef> = LazyLock::new(|| {
+    NamedNodeRef::new("http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#action").unwrap()
+});
+static NN_W3_RDFTEST_APPROVAL: LazyLock<NamedNodeRef> =
+    LazyLock::new(|| NamedNodeRef::new("http://www.w3.org/ns/rdftest#approval").unwrap());
+static NN_W3_RDFTEST_APPROVED: LazyLock<NamedNodeRef> =
+    LazyLock::new(|| NamedNodeRef::new("http://www.w3.org/ns/rdftest#Approved").unwrap());
 
-// fn get_remote_file(url: &str) -> Result<String> {
+const STR_W3_RDFTEST_TEST_TURTLE_EVAL: &str = "http://www.w3.org/ns/rdftest#TestTurtleEval";
+const STR_W3_RDFTEST_TEST_TURTLE_POSITIVE_SYNTAX: &str =
+    "http://www.w3.org/ns/rdftest#TestTurtlePositiveSyntax";
+const STR_W3_RDFTEST_TEST_TURTLE_NEGATIVE_SYNTAX: &str =
+    "http://www.w3.org/ns/rdftest#TestTurtleNegativeSyntax";
+static NN_W3_RDFTEST_TEST_TURTLE_EVAL: LazyLock<NamedNodeRef> =
+    LazyLock::new(|| NamedNodeRef::new(STR_W3_RDFTEST_TEST_TURTLE_EVAL).unwrap());
+static NN_W3_RDFTEST_TEST_TURTLE_POSITIVE_SYNTAX: LazyLock<NamedNodeRef> =
+    LazyLock::new(|| NamedNodeRef::new(STR_W3_RDFTEST_TEST_TURTLE_POSITIVE_SYNTAX).unwrap());
+static NN_W3_RDFTEST_TEST_TURTLE_NEGATIVE_SYNTAX: LazyLock<NamedNodeRef> =
+    LazyLock::new(|| NamedNodeRef::new(STR_W3_RDFTEST_TEST_TURTLE_NEGATIVE_SYNTAX).unwrap());
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("No type for test subject {0}")]
+    NoType(String),
+
+    #[error("No action for test subject {0}")]
+    NoAction(String),
+
+    #[error("The formatted graph for subject '{0}' is not the same as the expected graph.\nOriginal:\n'{1}'\n\nFormatted:\n'{2}'")]
+    DiffersFromExpected(String, Graph, Graph),
+
+    #[error(
+        "The formatting for subject '{0}' is not stable.\nFormatted:\n'{1}'\n\nReformatted:\n'{2}'"
+    )]
+    UnstableFormatting(String, String, String),
+
+    #[error("The content for subject '{0}' has been parsed without error, even though it should fail!\nContent:\n'{1}'")]
+    FalsePositive(String, String),
+
+    #[error("Failed to parse content as turtle: {0}\ncontent:\n'{1}'")]
+    Parsing(#[source] ParsingError, String),
+
+    #[error("Failed to format content as turtle: {0}\ncontent:\n'{1}'")]
+    Formatting(#[source] FmtError, String),
+
+    #[error("Failed to read a file: {0}")]
+    IO(#[from] std::io::Error),
+}
+
+fn format_turtle(original: &str, options: FormatOptions) -> Result<String, Error> {
+    let options = Rc::new(options);
+    let input = parser::parse(original.as_bytes(), &options)
+        .map_err(|parser_err| Error::Parsing(parser_err, original.to_owned()))?;
+    format(&input, options).map_err(|fmt_err| Error::Formatting(fmt_err, original.to_owned()))
+}
+
+// fn get_remote_file(url: &str) -> BoxResult<String> {
 //     let mut hasher = DefaultHasher::new();
 //     hasher.write(url.as_bytes());
 //     let cache_path = Path::new(CACHE).join(hasher.finish().to_string());
@@ -28,84 +84,160 @@
 //     Ok(content)
 // }
 
-// fn parse_turtle(url: &str, data: &str) -> Result<Graph> {
-//     TurtleParser::new()
-//         .with_base_iri(url)?
-//         .for_slice(data.as_bytes())
-//         .collect::<core::result::Result<_, _>>()
-//         .with_context(|| format!("Error while parsing:\n{data}"))
-// }
+fn parse_turtle(base: &str, data: &str) -> Result<Graph, ParsingError> {
+    TurtleParser::new()
+        // .with_quoted_triples()
+        .with_base_iri(base)
+        .map_err(ParsingError::BaseIri)?
+        .for_slice(data.as_bytes())
+        .collect::<core::result::Result<_, _>>()
+        .map_err(std::convert::Into::into)
+}
 
-// fn run_test(test: SubjectRef<'_>, manifest: &Graph) -> Result<()> {
-//     let Some(TermRef::NamedNode(test_type)) =
-//         manifest.object_for_subject_predicate(test, rdf::TYPE)
-//     else {
-//         bail!("No type")
-//     };
+fn options() -> FormatOptions {
+    FormatOptions {
+        check: false,
+        indentation: "  ".to_string(),
+        single_leafed_new_lines: false,
+        force: false,
+        prtr_sorting: false,
+        sparql_syntax: false,
+        max_nesting: true,
+        canonicalize: true,
+        warn_unsupported_numbers: true,
+    }
+}
 
-//     let Some(TermRef::NamedNode(input_url)) = manifest.object_for_subject_predicate(
-//         test,
-//         NamedNodeRef::new("http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#action")?,
-//     ) else {
-//         bail!("No action")
-//     };
-//     let original = get_remote_file(input_url.as_str())?.replace('\0', "");
-//     let formatted_result = format_turtle(&original, &FormatOptions::default());
+fn run_test(
+    manifest_dir_url: &str,
+    manifest_local_path_dir: &str,
+    test: NamedOrBlankNodeRef<'_>,
+    manifest: &Graph,
+) -> Result<(), Error> {
+    let Some(TermRef::NamedNode(test_type)) =
+        manifest.object_for_subject_predicate(test, rdf::TYPE)
+    else {
+        return Err(Error::NoType(test.to_string()));
+    };
 
-//     match test_type.as_str() {
-//         "http://www.w3.org/ns/rdftest#TestTurtleEval"
-//         | "http://www.w3.org/ns/rdftest#TestTurtlePositiveSyntax" => {
-//             let formatted = formatted_result?;
-//             let mut original_graph = parse_turtle(input_url.as_str(), &original)?;
-//             original_graph.canonicalize(CanonicalizationAlgorithm::Unstable);
-//             let mut formatted_graph = parse_turtle(input_url.as_str(), &formatted)?;
-//             formatted_graph.canonicalize(CanonicalizationAlgorithm::Unstable);
-//             if original_graph != formatted_graph {
-//                 bail!("The formatted graph is not the same as the original graph.\nOriginal:\n{}\n\nFormatted:\n{}", original, formatted);
-//             }
-//             let reformatted = format_turtle(&formatted, &FormatOptions::default())?;
-//             if formatted != reformatted {
-//                 bail!(
-//                     "The formatting is not stable.\nOriginal:\n{}\n\nReformatted:\n{}",
-//                     formatted,
-//                     reformatted
-//                 );
-//             }
-//             Ok(())
-//         }
-//         "http://www.w3.org/ns/rdftest#TestTurtleNegativeSyntax" => {
-//             if formatted_result.is_ok() {
-//                 bail!(
-//                     "The following file has been parsed without error even if it should fail:\n{}",
-//                     original
-//                 )
-//             }
-//             Ok(())
-//         }
-//         _ => Ok(()),
-//     }
-// }
+    let Some(TermRef::NamedNode(input_url)) =
+        manifest.object_for_subject_predicate(test, *NN_W3C_TESTS_ACTION)
+    else {
+        return Err(Error::NoAction(test.to_string()));
+    };
+    let input_local_path = input_url
+        .as_str()
+        .replace(manifest_dir_url, manifest_local_path_dir);
+    let original = fs::read_to_string(&input_local_path)?.replace('\0', "");
+    let formatted_result = format_turtle(&original, options());
+
+    match test_type.as_str() {
+        STR_W3_RDFTEST_TEST_TURTLE_EVAL | STR_W3_RDFTEST_TEST_TURTLE_POSITIVE_SYNTAX => {
+            let formatted = formatted_result?;
+            let input_url = "http://a.a".to_string(); // TODO HACK FIXME
+            let mut original_graph = parse_turtle(input_url.as_str(), &original)
+                .map_err(|parser_err| Error::Parsing(parser_err, original.clone()))?;
+            original_graph.canonicalize(CanonicalizationAlgorithm::Unstable);
+            let mut formatted_graph = parse_turtle(input_url.as_str(), &formatted)
+                .map_err(|parser_err| Error::Parsing(parser_err, formatted.clone()))?;
+            formatted_graph.canonicalize(CanonicalizationAlgorithm::Unstable);
+            if original_graph != formatted_graph {
+                return Err(Error::DiffersFromExpected(
+                    test.to_string(),
+                    original_graph,
+                    formatted_graph,
+                ));
+            }
+            let reformatted = format_turtle(&formatted, FormatOptions::default())?;
+            if formatted != reformatted {
+                return Err(Error::UnstableFormatting(
+                    test.to_string(),
+                    formatted,
+                    reformatted,
+                ));
+            }
+            Ok(())
+        }
+        STR_W3_RDFTEST_TEST_TURTLE_NEGATIVE_SYNTAX => {
+            if formatted_result.is_ok() {
+                return Err(Error::FalsePositive(test.to_string(), original));
+            }
+            Ok(())
+        }
+        _ => Ok(()),
+    }
+}
+
+fn test_w3c_test_suite(
+    manifest_dir_url: &str,
+    manifest_local_path_dir: &str,
+    approved_only: bool,
+) -> Result<(), Error> {
+    let manifest_url = format!("{manifest_dir_url}/manifest.ttl");
+    let manifest_local_path = format!("{manifest_local_path_dir}/manifest.ttl");
+    let manifest_content = fs::read_to_string(manifest_local_path)?;
+    let manifest_rdf = parse_turtle(&manifest_url, &manifest_content)
+        .map_err(|parser_err| Error::Parsing(parser_err, manifest_content.clone()))?;
+    let errors = if approved_only {
+        manifest_rdf
+            .subjects_for_predicate_object(*NN_W3_RDFTEST_APPROVAL, *NN_W3_RDFTEST_APPROVED)
+            .chain(vec![])
+            .collect::<Vec<_>>()
+    } else {
+        manifest_rdf
+            .subjects_for_predicate_object(rdf::TYPE, *NN_W3_RDFTEST_TEST_TURTLE_EVAL)
+            .chain(manifest_rdf.subjects_for_predicate_object(
+                rdf::TYPE,
+                *NN_W3_RDFTEST_TEST_TURTLE_POSITIVE_SYNTAX,
+            ))
+            .chain(manifest_rdf.subjects_for_predicate_object(
+                rdf::TYPE,
+                *NN_W3_RDFTEST_TEST_TURTLE_NEGATIVE_SYNTAX,
+            ))
+            .collect::<Vec<_>>()
+    }
+    .into_iter()
+    .filter_map(|t| {
+        run_test(manifest_dir_url, manifest_local_path_dir, t, &manifest_rdf)
+            .err()
+            .map(|err| err.to_string())
+    })
+    .collect::<Vec<_>>();
+    // let errors = manifest_rdf
+    //     .subjects_for_predicate_object(*NN_W3_RDFTEST_APPROVAL, *NN_W3_RDFTEST_APPROVED)
+    //     .filter_map(|t| {
+    //         run_test(manifest_dir_url, manifest_local_path_dir, t, &manifest_rdf)
+    //             .err()
+    //             .map(|err| err.to_string())
+    //     })
+    //     .collect::<Vec<_>>();
+    assert!(errors.is_empty(), "{}", errors.join("\n"));
+    Ok(())
+}
+
+#[test]
+fn test_w3c_modern_rdf11() -> Result<(), Error> {
+    test_w3c_test_suite(
+        "http://w3c.github.io/rdf-tests/rdf/rdf11/rdf-turtle",
+        "tests/data/input/w3c/modern/rdf11",
+        true,
+    )
+}
 
 // #[test]
-// fn test_w3c_files() -> Result<()> {
-//     fs::create_dir_all(CACHE)?; // We ensure cache existence
+fn test_w3c_modern_rdf12_eval() -> Result<(), Error> {
+    test_w3c_test_suite(
+        "http://w3c.github.io/rdf-tests/rdf/rdf12/rdf-turtle/eval",
+        "tests/data/input/w3c/modern/rdf12/eval",
+        false,
+    )
+}
 
-//     let manifest = parse_turtle(
-//         "http://w3c.github.io/rdf-tests/rdf/rdf11/rdf-turtle/manifest.ttl",
-//         &get_remote_file("http://w3c.github.io/rdf-tests/rdf/rdf11/rdf-turtle/manifest.ttl")?,
-//     )?;
-//     let errors = manifest
-//         .subjects_for_predicate_object(
-//             NamedNodeRef::new("http://www.w3.org/ns/rdftest#approval")?,
-//             NamedNodeRef::new("http://www.w3.org/ns/rdftest#Approved")?,
-//         )
-//         .filter_map(|t| {
-//             run_test(t, &manifest)
-//                 .with_context(|| format!("{t} failed"))
-//                 .map_err(|e| format!("{e:?}"))
-//                 .err()
-//         })
-//         .collect::<Vec<_>>();
-//     assert!(errors.is_empty(), "{}", errors.join("\n"));
-//     Ok(())
-// }
+// #[test]
+fn test_w3c_modern_rdf12_syntax() -> Result<(), Error> {
+    test_w3c_test_suite(
+        "http://w3c.github.io/rdf-tests/rdf/rdf12/rdf-turtle/syntax",
+        "tests/data/input/w3c/modern/rdf12/syntax",
+        false,
+    )
+}
