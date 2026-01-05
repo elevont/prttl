a80c80749a2365a0f4ac0ba0c280f84a7d8333a2
Robin Vobruba <hoijui.quaero@gmail.com>
2026-01-05 20:14:47 +0100

    Do not require input files when --version is present [fix]

--- src/cli.rs
+++ src/cli.rs
@@ -336,7 +336,7 @@ fn arg_src() -> Arg {
         .value_name("FILE_OR_DIR")
         .value_hint(ValueHint::Other)
         .value_parser(value_parser!(PathBuf))
-        .required(true)
+        .required_unless_present(A_L_VERSION)
         .action(ArgAction::Set)
 }
 
