f3735f3fb0db4d5f5bb0a65b2442ae89f39f48c0
Robin Vobruba <hoijui.quaero@gmail.com>
2026-01-05 20:14:01 +0100

    Nicer --version help message [minor]

--- src/cli.rs
+++ src/cli.rs
@@ -321,7 +321,7 @@ fn arg_version() -> Arg {
     Arg::new(A_L_VERSION)
         .help(formatcp!(
             "Print version information and exit. \
-May be combined with -{A_S_QUIET},--{A_L_QUIET}, \
+May be combined with -{A_S_QUIET}/--{A_L_QUIET}, \
 to really only output the version string."
         ))
         .short(A_S_VERSION)
